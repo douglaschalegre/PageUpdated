2ec49240ea0370dde0950c491c4c9e5987072d95
douglaschalegre <dougchalegre@hotmail.com>
2020-04-22 09:40:35 -0300

    Notificação do telegram implementada & alterado a forma de busca por mudança na pagina

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python
 import time
+import requests
+
+def sendText(botMessage):
+    botToken = ''
+    botChatId = ''
+    sendTxt = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + botChatId + '&parse_mode=Markdown&text=' + botMessage
+    response = requests.get(sendTxt)
+
+    return response.json()
 
 counter = 0
-
 while True:
-    f1=open("content.txt","r")
-    f2=open("contentaux.txt","r")
-    for line1 in f1:
-        for line2 in f2:
-            if line1==line2:
-                print("SAME")
-            else:
-                print("NOT SAME")
-            break
-    counter += 1
     print(50*'=')
     print('Run number: {}'.format(counter))
     print(50*'=')
-    time.sleep(1)
+    f1= open("content.txt","r")
+    f2= open("contentaux.txt","r")
+    lines1 = f1.readlines()
+    lines2 = f2.readlines()
+
+    sendNotification = False
+
+    try:
+        if lines1[0]==lines2[0]:
+            print("Nenhuma mudança por aqui!")
+        else:
+            sendNotification = True
+            print("[+] Text Sent!!")
+            print("content.txt: {}".format(lines1[0]))
+            print("contentaux.txt: {}".format(lines2[0]))
+    except:
+        print("Oopsie! Probably writing file...")
+            
+
+    if sendNotification == True:
+        sendText('Ei! Algo mudou... Fica de olho!')   
+
+    counter += 1
+    time.sleep(10)
     f1.close()
     f2.close()
+
--- getchamados.sh
+++ getchamados.sh
@@ -4,14 +4,14 @@ while true
 do
     if [ "$boolean" = true ]
     then
-        curl -v --cookie "IF PAGE HAVE COOKI PUT HERE" [https://example.com/] | grep "[What you want to track]" > content.txt;
+        curl -v --cookie "COOKIE" link | grep "content" > content.txt;
         echo
         echo '=============================================='
         echo '============ Updated content.txt ============'
         echo '=============================================='
         boolean=false
     else
-        curl -v --cookie "IF PAGE HAVE COOKI PUT HERE" [https://example.com/] | grep "[What you want to track]" > contentaux.txt;
+        curl -v --cookie "COOKIE" link | grep "content" > contentaux.txt;
         echo '=============================================='
         echo '========== Updated contentaux.txt ==========='
         echo '=============================================='
