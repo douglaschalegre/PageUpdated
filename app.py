d5b82ecf2386ac5fa3a04a4513558a30aec46953
douglaschalegre <dougchalegre@hotmail.com>
2020-05-15 02:51:14 -0300

    Personalized for TJAL

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python
-import time
-import sys
+import time, sys, os
+import datetime
 import requests
-import os
 
 def sendText(botMessage):
-    botToken = '1173827382:AAGd13RVAbBRcVWNpcXAZHni-uEc7rdAS6E'
-    botChatId = '842357098' #meu id
-    #botChatId = '-300263333' #group id
+    botToken = ''
+    botChatId = '' #meu id
+    #botChatId = '' #group id
     sendTxt = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + botChatId + '&parse_mode=Markdown&text=' + botMessage
     response = requests.get(sendTxt)
 
     return response.json()
-
-def latestFile(file1,file2):
-    infoFile1 = os.stat(file1)[9] # [9] acesses the st_mtime from the object
-    infoFile2 = os.stat(file2)[9] # which is the timestamp for file modification
-    if infoFile1 > infoFile2: #higher means most recently modified
-        return lines1[0]
-    else:
-        return lines2[0]
-
+    
 def progressbar(it, prefix="", size=60, file=sys.stdout):
     count = len(it)
     def show(j):
@@ -33,6 +24,41 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
         show(i+1)
     file.write("\n")
     file.flush()
+
+def mostRecent(file1,file2):
+    infoFile1 = os.stat(file1)[9] # [9] acesses the st_mtime from the object
+    infoFile2 = os.stat(file2)[9] # which is the timestamp for file modification
+    if infoFile1 > infoFile2: #higher means most recently modified
+        return lines1
+    else:
+        return lines2
+
+def getTicket(file1,file2):
+        splitedWebPage = mostRecent(file1, file2)[1].split("'")
+        return splitedWebPage[3]
+
+def makeDatetime(line):
+    splitedWebPage = line[8]
+    splitedWebPage = splitedWebPage.split(">")
+    splitedWebPage = splitedWebPage[1][0:-4]
+    splitedWebPage = splitedWebPage.split(" ")
+    splitedWebPage = [splitedWebPage[0].split("-"), splitedWebPage[1].split(":")]
+    # datetime(year, month, day, hour, minute, second) 
+    year = int(splitedWebPage[0][2])
+    month = int(splitedWebPage[0][1])
+    day = int(splitedWebPage[0][0])
+    hour = int(splitedWebPage[1][0])
+    minute = int(splitedWebPage[1][1])    
+    return datetime.datetime(year,month,day,hour,minute,0)
+
+def getTime(line1,line2):
+    a = makeDatetime(line1)
+    b = makeDatetime(line2)
+    if a>b:
+        return a
+    elif b>a:
+        return b
+
 
 counter = 0
 while True:
@@ -59,11 +85,14 @@ while True:
             
 
     if sendNotification == True:
-        sendText('Olha só, o seguinte chamado foi aberto agora! \n {}'.format(latestFile('content.txt','contentaux.txt')))   
+        ticket = getTicket('content.txt','contentaux.txt')
+        lastAtt = getTime(lines1,lines2)
+        description = 0
+        sendText('**Atenção!** o {} foi atualizado agora as {} \n {}'.format(ticket, lastAtt, description))   
 
     counter += 1
-    for i in progressbar(range(10), "Waiting: ", 13):
-        time.sleep(1) # any calculation you need
+    for i in progressbar(range(10), "Waiting: ", 20):
+        time.sleep(1)
+
     f1.close()
     f2.close()
-
--- getchamados.sh
+++ getchamados.sh
@@ -1,22 +1,17 @@
 boolean=true
-cookie="glpi_f8245b57fcd351174c77313b7def86a1=tqn5sns2gadniajhhai8udsep2; glpi_f8245b57fcd351174c77313b7def86a1_rememberme=%5B%225652%22%2C%22%242y%2410%24pAHPGciGQb9hv%5C%2FXolKPIfubSwz0UzQu9MDUI3mGt3HtCeNhKlPT5y%22%5D"
+cookie=""
 while true
 do
     if [ "$boolean" = true ]
     then
-        curl -v --cookie $cookie http://portalti.tjal.jus.br/glpi/front/ticket.php | grep "id='Ticket" > content.txt;
-        echo
-        echo '=============================================='
-        echo '============ Updated content.txt ============'
-        echo '=============================================='
+        curl -v --cookie $cookie http://example.com | grep "example\|example2" > content.txt;
+        echo '[+] Content.txt updated'
         boolean=false
     else
-        curl -v --cookie $cookie http://portalti.tjal.jus.br/glpi/front/ticket.php | grep "id='Ticket" > contentaux.txt;
-        echo '=============================================='
-        echo '========== Updated contentaux.txt ==========='
-        echo '=============================================='
+        curl -v --cookie $cookie http://example.com | grep "example\|example2" > contentaux.txt;
+        echo '[+] ContentAux.txt updated'
         boolean=true
     fi
-    sleep 10
+    sleep 30
 done
 
